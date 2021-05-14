bb5f2770761b892d6a8c3be437dbff350e99d9d1
Gintare B <gintaar3@gmail.com>
2021-05-14 14:00:17 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
 import GettingStarted from './components/GettingStarted/GettingStarted'
+import Nav from './components/Nav/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <GettingStarted/>
     </div>
   );
